08f5a86c50796d002dfd19a688fc9e4666ce95e7
Aaron Neff <arneff@gmail.com>
2018-01-25 18:03:57 -0700

    updated lat lng

--- js/app.js
+++ js/app.js
@@ -16,11 +16,11 @@ function initMap() {
   });
 
   let locations = [
-    {name: 'Iron Works BBQ', location: {lat: 30.2622, lng: -97.7390}},
-    {name: 'Franklin Barbecue', location: {lat: 30.270142, lng: -97.7334599}},
-    {name: 'La Barbecue', location: {lat:30.2561, lng: -97.7223}},
-    {name: "Stubb's Bar-B-Q", location: {lat: 30.2685, lng: -97.7363}},
-    {name: 'Kerlin BBQ', location: {lat: 30.2627011, lng: -97.7345971}}
+    {name: 'Iron Works BBQ', location: {lat: 30.2622277, lng: -97.7411904}},
+    {name: 'Franklin Barbecue', location: {lat: 30.2701466, lng: -97.7356486}},
+    {name: 'La Barbecue', location: {lat:30.2561453, lng: -97.7245339}},
+    {name: "Stubb's Bar-B-Q", location: {lat: 30.2685053, lng: -97.7384466}},
+    {name: 'Kerlin BBQ', location: {lat: 30.2578579, lng: -97.7278947}}
   ];
 
   let largeInfowindow = new google.maps.InfoWindow();
